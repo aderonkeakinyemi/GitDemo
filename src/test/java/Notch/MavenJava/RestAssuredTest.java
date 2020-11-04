692c706f50b5beffbda72aaf1e06ddcc54cae830
= <=>
2020-11-04 05:15:58 +0100

    This is my second commit

--- src/test/java/Notch/MavenJava/RestAssuredTest.java
+++ src/test/java/Notch/MavenJava/RestAssuredTest.java
@@ -8,6 +8,9 @@ public class RestAssuredTest {
 	@Test
 	public void twoFA() {
 		System.out.println("It is a 2FA Test");
+		System.out.println("It is a 2FA Test1");
+		System.out.println("It is a 2FA Test2");
+		System.out.println("It is a 2FA Test3");
 	}
 	
 	@Test
